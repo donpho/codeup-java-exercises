0b9f554af7deccd8d45f373168e8bc3d843bfe82
Don Pho <donpho@Dons-MacBook-Air.local>
2018-07-23 14:12:01 -0500

    java syntax intro exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,39 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("hello");
+
+        int myFavoriteNumber = 1004;
+        String myString = "hello there";
+        double number = 3.14;
+
+//        System.out.println(number);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        int x = 4;
+////        x = x + 5;
+//        x+=5;
+//        System.out.println(x);
+
+//        int x = 3, y = 4;
+////        y = y * x;
+//        y *= x;
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+//        x = x / y;
+//        y = y - x;
+        x /= y;
+        y -= x;
+        System.out.println(x);
+        System.out.println(y);
+
     }
 }
